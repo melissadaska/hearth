7537df5760400d104ce2ccf279103c8f18c8fb39
Brent F <feuerbacherb@gmail.com>
2020-06-25 10:55:46 -0500

    created join tables for user_group and post_comment

--- models/index.js
+++ models/index.js
@@ -3,6 +3,27 @@ const Group = require('./Group');
 const Post = require('./Post');
 const User = require('./User');
 
+// create dynamic join tables
+// User_Group table
+User.belongsToMany(Group, {
+   through: 'User_Group'
+});
+
+Group.belongsToMany(User, {
+   through: 'User_Group'
+});
+
+// Post_Comment table
+Post.belongsToMany(Comment, {
+   through: 'Post_Comment'
+});
+
+Comment.belongsToMany(Post, {
+   through: 'Post_Comment'
+});
+
+// Picture_Comment table
+
 
 module.exports = {
    Comment,
